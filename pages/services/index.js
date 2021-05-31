acba21053ae8888029ce9684e78b99fab7b1b17e
shahnewaz601 <shahnewaz601@gmail.com>
2021-05-31 23:56:59 +0600

    simple update

--- pages/services/index.js
+++ pages/services/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const index = () => {
     return (
         <div>
-            <h1>This is service main page</h1>
+            <h1>This is service page</h1>
             <a href="/services/repair">Repair</a>
         </div>
     );
